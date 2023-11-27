6351f3d4e7d737abf75dd519cfc90f3b8b1f1c7a
tonygril <antomore353@hotmail.com>
2023-11-27 15:53:33 -0500

    agregando sistema de recomendacion optimizado

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException, Query, Depends
 from typing import Dict, List, Union
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
@@ -216,13 +216,15 @@ def sentiment_analysis(developer: str = Query(..., title="Empresa desarrolladora
         return result_list
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
-df_items_unicos = df[['item_id', 'item_name', 'Action', 'Adventure', 'Indie', 'RPG', 'Strategy']]
-df_items_unicos = df_items_unicos.drop_duplicates(subset='item_id')
-df_items_unicos.set_index('item_id', inplace=True)
 
-# Excluir la columna 'item_name' antes de calcular la similitud del coseno
-df_similitud = cosine_similarity(df_items_unicos.drop('item_name', axis=1))
+
+from fastapi import Depends, HTTPException, Query
+from typing import List
+
+# Obtén los datos una sola vez y utilízalos en las funciones que los necesiten
+df_items_unicos = df[['item_id', 'item_name', 'Action', 'Indie']]
+df_items_unicos.set_index('item_id', inplace=True)
+df_similitud = cosine_similarity(df_items_unicos[['Action', 'Indie']])
 
 @app.get('/recommend_games', tags=['RecommendGames'])
 def recommend_games(item_id: int = Query(..., title="ID del juego")) -> List[str]:
@@ -244,23 +246,17 @@ def recommend_games(item_id: int = Query(..., title="ID del juego")) -> List[str
         if item_id not in df_items_unicos.index:
             raise HTTPException(status_code=404, detail=f"No se encontró información para el juego con item_id '{item_id}'")
         
-        # Obtener el género asociado al 'item_id'
-        genre = df_items_unicos.loc[item_id, ['Action', 'Adventure', 'Indie', 'RPG']].idxmax()
-
-        # Obtener la fila de similitud correspondiente
+        # Obtener la fila de similitud correspondiente solo para las columnas 'Action' y 'Indie'
         fila_similitud = df_similitud[df_items_unicos.index.get_loc(item_id)]
-        
+
         # Obtener los 'item_id' recomendados ordenados por similitud en orden descendente y seleccionar los primeros n
-        juegos_recomendados = df_items_unicos.index[fila_similitud.argsort()[::-1][:5]]
-        
-        # Filtrar los juegos recomendados por el género asociado al 'item_id'
-        juegos_recomendados = [juego for juego in juegos_recomendados if df_items_unicos.loc[juego, genre] == 1]
-        
+        juegos_recomendados = df_items_unicos.index[fila_similitud.argsort()[::-1][:5]].tolist()
+
         # Obtener los 'item_name' correspondientes a los 'item_id' recomendados
         juegos_recomendados_nombres = df_items_unicos.loc[juegos_recomendados, 'item_name'].tolist()
         
         return juegos_recomendados_nombres
+    except KeyError as ke:
+        raise HTTPException(status_code=500, detail=f"Error de clave: {str(ke)}")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
-    
